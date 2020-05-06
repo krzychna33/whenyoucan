49513489794f23b46427ca91568d579ba93e1c69
Krzysztof Surażyński <krzychna33@gmail.com>
2020-05-07 01:16:21 +0200

    env changes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,13 @@
 const merge = require('webpack-merge');
+const webpack = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.API_URL': JSON.stringify(process.env.API_URL),
+            'process.env.APP_URL': JSON.stringify(process.env.APP_URL)
+        })
+    ]
 });
